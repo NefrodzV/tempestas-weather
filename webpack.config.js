c875f9a2f2a04f977a42d13b6ed3c6c6dbbfaf89
Neftaly <neftalyrv23@gmail.com>
2023-08-21 10:46:49 -0400

    Set the webpack config to run styles and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,4 +7,16 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+}
