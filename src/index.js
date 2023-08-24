42c63456921af99ce1f436ab99daf641a5f09eca
Neftaly <neftalyrv23@gmail.com>
2023-08-24 12:05:19 -0400

    Added font to body

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import "./style.css"
 console.log("Hello Tempestas")
-
-let nums = [1, 2, 3, 4, 5, 6, 5]
-
-let isLessThan1Ten = nums.every((num) => typeof num === "number")
-console.log(isLessThan1Ten)
