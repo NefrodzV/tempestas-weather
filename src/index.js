104f1d05f1e226a289df22e8e8db00fa7a45c1b5
Neftaly <neftalyrv23@gmail.com>
2023-08-25 10:09:01 -0400

    Loaded the default images with js to html and did validation to the search

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 import "./style.css"
+import logoSvg from "./assets/icons/icon-goddess-logo.svg"
+import discordSvg from "./assets/icons/icon-discord.svg"
+import chevronLeftSvg from "./assets/icons/chevron-left.svg"
+import chevronRightSvg from "./assets/icons/chevron-right.svg"
+import uvSvg from "./assets/icons/icon-uv.svg"
+import sunsetSvg from "./assets/icons/icon-sunset.svg"
+import humiditySvg from "./assets/icons/icon-humidity.svg"
+import windsSvg from "./assets/icons/icon-winds.svg"
+
+import { getElement, isBlank, loadImage, showErrorMessage } from "./utils/Utils"
 console.log("Hello Tempestas")
+
+// Setting the default images of the page
+const logoImage = getElement(".logo img")
+const discordImage = getElement("#discord-link img")
+const leftButtonImg = getElement("#left img")
+const rightButtonImg = getElement("#right img")
+const uvImage = getElement("#uv-img")
+const humidityImg = getElement("#humidity-img")
+const windsImg = getElement("#winds-img")
+const sunsetImg = getElement("#sunset-img")
+
+loadImage(logoImage, logoSvg)
+loadImage(discordImage, discordSvg)
+loadImage(leftButtonImg, chevronLeftSvg)
+loadImage(rightButtonImg, chevronRightSvg)
+loadImage(uvImage, uvSvg)
+loadImage(humidityImg, humiditySvg)
+loadImage(windsImg, windsSvg)
+loadImage(sunsetImg, sunsetSvg)
+
+const handleEnter = (event) => {
+  const parent = event.target
+  if (
+    event.key === "Enter" &&
+    (parent.validity.valueMissing || isBlank(parent.value))
+  ) {
+    showErrorMessage(parent)
+    //reset the caret
+    parent.value = ""
+  }
+
+  // Search the term
+}
+
+const searchInput = getElement("#search")
+searchInput.addEventListener("keydown", handleEnter)
+
+const buttonRight = getElement("#right")
+console.log(buttonRight)
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,14 +1,28 @@
 // Utility functions
-export const getElement = (selector) => {
+const getElement = (selector) => {
   const element = document.querySelector(selector)
-  if (element === null) {
-    return console.log(
-      "Searched element with selector: " + selector + "is null"
-    )
-  }
+  // if (element === null) {
+  //   return console.log(
+  //     "Searched element with selector: " + selector + "is null"
+  //   )
+  // }
+  console.log(element)
   return element
 }
 
-export const loadImage = (img, path) => {
+const loadImage = (img, path) => {
   img.src = path
 }
+
+const showErrorMessage = (parent) => {
+  console.log(parent)
+  const errorElement = parent.nextElementSibling
+  if (errorElement.hasAttribute("show")) return
+  errorElement.toggleAttribute("show")
+}
+
+const isBlank = (string) => {
+  return string.trim() === ""
+}
+
+export { getElement, loadImage, showErrorMessage, isBlank }
