b24e080afa200b7d8f992e71a80b4c8b20aaabbf
Neftaly <neftalyrv23@gmail.com>
2023-08-28 19:33:15 -0400

    Chnaged to function object

--- src/data/model/CurrentWeatherModel.js
+++ src/data/model/CurrentWeatherModel.js
@@ -3,33 +3,31 @@ import { WeatherConditions } from "./WeatherConditions"
 
 // TODO: Change this to get the condition code and chnage the condition to text
 // Adding properties like contion description, condition code, condtion img or icon
-export class CurrentWeatherModel {
-  constructor(
-    city,
-    region,
-    localTime,
-    temperatureFarenheit,
-    isDay,
-    code,
-    windsMph,
-    humidity,
-    feelsLikeFarenheit,
-    uvIndex,
-    sunrise,
-    sunset
-  ) {
-    this.city = city
-    this.region = region
-    this.localTime = localTime
-    this.temperatureFarenheit = roundNumber(temperatureFarenheit)
-    this.isDay = isDay
-    this.code = code
-    this.condition = WeatherConditions.getCondition(code)
-    this.windsMph = roundNumber(windsMph)
-    this.humidity = humidity
-    this.feelsLikeFarenheit = roundNumber(feelsLikeFarenheit)
-    this.uvIndex = formatUvIndex(uvIndex)
-    this.sunrise = sunrise
-    this.sunset = sunset
-  }
+export default function CurrentWeatherModel(
+  city,
+  region,
+  localTime,
+  temperatureFarenheit,
+  isDay,
+  code,
+  windsMph,
+  humidity,
+  feelsLikeFarenheit,
+  uvIndex,
+  sunrise,
+  sunset
+) {
+  this.city = city
+  this.region = region
+  this.localTime = localTime
+  this.temperatureFarenheit = roundNumber(temperatureFarenheit)
+  this.isDay = isDay
+  this.code = code
+  this.condition = WeatherConditions.getCondition(code)
+  this.windsMph = roundNumber(windsMph)
+  this.humidity = humidity
+  this.feelsLikeFarenheit = roundNumber(feelsLikeFarenheit)
+  this.uvIndex = formatUvIndex(uvIndex)
+  this.sunrise = sunrise
+  this.sunset = sunset
 }
